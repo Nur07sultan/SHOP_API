863d1b8e432881e6d7e93fd9cf15c46a1fa2e0f9
Nur07sultan <Aibeknurel07@gmail.com>
2025-10-26 21:45:17 +0600

    first commit

---
+++ product/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from .models import Category, Product, Review
+
+# Регистрируем модели в админке, чтобы можно было добавлять/редактировать записи
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	# показывать id и название в списке
+	list_display = ('id', 'name')
+	# поле для поиска
+	search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	# показывать id, заголовок, цену и категорию
+	list_display = ('id', 'title', 'price', 'category')
+	# фильтр по категории
+	list_filter = ('category',)
+	# поиск по заголовку и описанию
+	search_fields = ('title', 'description')
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+	# показывать id, текст и связанный товар
+	list_display = ('id', 'text', 'product')
+	search_fields = ('text',)
+
+
+# Настройки интерфейса админки на русском
+admin.site.site_header = 'Администрация магазина'
+admin.site.site_title = 'Админка магазина'
+admin.site.index_title = 'Управление моделями магазина'
+
---
+++ product/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_('название'))
+
+    class Meta:
+        verbose_name = _('Категория')
+        verbose_name_plural = _('Категории')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, verbose_name=_('заголовок'))
+    description = models.TextField(verbose_name=_('описание'))
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('цена'))
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name=_('категория'))
+
+    class Meta:
+        verbose_name = _('Товар')
+        verbose_name_plural = _('Товары')
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    text = models.TextField(verbose_name=_('текст'))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews', verbose_name=_('товар'))
+
+    class Meta:
+        verbose_name = _('Отзыв')
+        verbose_name_plural = _('Отзывы')
+
+    def __str__(self):
+        return f"{self.text[:50]}"
+
